4f4596ec55f11c1ab54d03e91b4c73dd308a21e6
asyayu <yurchenkoasya@gmail.com>
2023-04-21 12:48:27 +0200

    Change data-transform file path

--- data-transform.py
+++ data-transform.py
@@ -1,7 +1,7 @@
 import pandas
 
-data = pandas.read_csv("/Users/asya/Downloads/web-development/projects/horror-api/movies.csv")
+data = pandas.read_csv("movies.csv")
 
 data['year'] = data['title'].str[-5:-1]
 
-data.to_csv("/Users/asya/Downloads/web-development/projects/horror-api/movies_new.csv")
+data.to_csv("movies_new.csv")
